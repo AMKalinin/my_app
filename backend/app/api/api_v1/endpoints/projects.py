ce0c89844a68b5b1c4a0216c38adf9f752cd5ae6
Фролов Иван Ильич <ifrolov@npomis.ru>
2023-08-23 11:44:26 +0300

    crud project

--- backend/app/api/api_v1/endpoints/projects.py
+++ backend/app/api/api_v1/endpoints/projects.py
@@ -12,3 +12,19 @@ router = APIRouter()
 def create_project(*, db: Session = Depends(deps.get_db), project_in: CreateProject) -> Any:
     project = crud.project.create(db, project_in)
     return project
+
+
+@router.get('/g', response_model=CreateProject)
+def get_project(*, db: Session = Depends(deps.get_db), name:str) -> Any:
+    project = crud.project.get_by_name(db, name)
+    return project
+
+@router.put('/u', response_model=CreateProject)
+def update_project(*, db: Session = Depends(deps.get_db), project_in:CreateProject) -> Any:
+    project = crud.project.get_by_name(db, project_in.name)
+    crud.project.update(db, project, project_in)
+    return project
+
+@router.delete('/d')
+def delete_project(*, db: Session = Depends(deps.get_db), name:str) -> Any:
+    crud.project.delete_by_name(db, name)
--- backend/app/crud/crud_project.py
+++ backend/app/crud/crud_project.py
@@ -7,7 +7,7 @@ class CRUDProject():
     def create(self, db:Session, project_in:CreateProject)->Project:
         db_project = Project(name = project_in.name,
                              create_date = project_in.create_date,
-                             last_update_date = project_in.last_update_date,
+                             last_update = project_in.last_update,
                              creator = project_in.creator,
                              status = project_in.status,
                              description = project_in.description)
@@ -15,6 +15,24 @@ class CRUDProject():
         db.commit()
         db.refresh(db_project)
         return db_project
+    
+    def get_by_name(self, db:Session, name:str) -> Project:
+        return db.query(Project).filter(Project.name == name).first()
+    
+    def update(self, db:Session, db_project:Project, project_in:CreateProject) -> Project:
+        db_project.last_update = project_in.last_update
+        db.commit()
+        return db_project
+    
+    def delete(self, db:Session, db_project:Project):
+        db.delete(db_project)
+        db.commit()
+    
+    def delete_by_name(self, db:Session, name:str):
+        project = db.query(Project).get(name)
+        db.delete(project)
+        db.commit()
+
 
 project = CRUDProject() 
     
