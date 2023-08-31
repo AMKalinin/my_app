1a4b99b2a8eaa2bb8a51491bfdacd2c1219a5e16
Alexandr Kalinin <am1kalinin@gmail.com>
2023-08-31 16:07:37 +0300

    pytest

--- backend/app/tests/api/api_v1/test_projects.py
+++ backend/app/tests/api/api_v1/test_projects.py
@@ -1,10 +1,10 @@
+from time import sleep
 from fastapi.testclient import TestClient
-# from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session
 
 
-def test_read_main() -> None:
-    # response = client.get('api/v1/projects/')
-    # assert response.status_code == 200
-    # assert response.json() == {'stroka': 'ok'}
-    assert 2 == 2
-    assert 2 == 3
+def test_read_main(client: TestClient, db: Session) -> None:
+    response = client.get('api/v1/projects/')
+    assert response.status_code == 200
+    content = response.json()
+    assert content[0]['name'] == 'test'
